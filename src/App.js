1a1839320938d4ed6c119356e403b7ec7fbf3121
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-10-31 18:45:00 +0300

    ABI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {usetate} from 'react';
 import {Web3} from 'web3';
 
 //import the contract adress
+const ADRESS="0x102BcF1c61E56a690B355f7b9E2198a81EE9695c";
+const ABI=[{"inputs":[{"internalType":"uint256","name":"startingPoint","type":"uint256"},{"internalType":"string","name":"startingMessage","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"decreaseNumber","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getNumber","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"increaseNumber","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"message","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"newMessage","type":"string"}],"name":"setMessage","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
 function App() {
   return (
