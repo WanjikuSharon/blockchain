99a930d855dd6b3fe60811e6b6b7a0d48c435805
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-10-31 18:35:22 +0300

    oops

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {usetate} from 'react';
+
+//import web3module
+import {Web3} from 'web3';
+
+//import the contract adress
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button>Get Number</button>
+       <br />
+       <button>Get Message</button>
+       <br />
+       <button>Increase Number</button>
+       <br />
+       <button>Decrease Number</button>
+       <br />
+       <input />
+       <br />
+       <button>Update Message</button>
       </header>
     </div>
   );
